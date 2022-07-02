d81a655a5b80cfa40e483b6b7b78568f2a15653e
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-07-02 09:57:15 +0000

    Deploying to gh-pages from @ Reinghord/goit-team01-project@c9200bb7b0555d1b99a2df4c8c8f9edf1b13ec67 🚀

---
+++ index.810c69aa.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.810c69aa.js.map
---
+++ index.fa131d4d.js
@@ -0,0 +1,2 @@
+new Swiper(".swiper",{loop:!0,pagination:{el:".swiper-pagination"},navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"}});
+//# sourceMappingURL=index.fa131d4d.js.map
