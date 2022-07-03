137ffce24cd0d876b97f3d61a205a36b6399fe27
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-07-03 09:15:24 +0000

    Deploying to gh-pages from @ Reinghord/goit-team01-project@0bd70de27a4fb09a7dad7c59165e35fc09cf93c6 🚀

---
+++ index.72b40ae7.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-menu-button]"),t=document.querySelector("[data-menu]");e.addEventListener("click",(function(){var n="true"===e.getAttribute("aria-expanded")||!1;document.body.classList.toggle("menu-open"),e.classList.toggle("is-open"),e.setAttribute("aria-expanded",!n),t.classList.toggle("is-open")}))}();
+//# sourceMappingURL=index.72b40ae7.js.map
