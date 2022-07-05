17ec1cc7e3bb8d7bb347f952f93f7b0d0fad5492
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-07-05 14:10:00 +0300

    added scroll

---
+++ src/js/scroll.js
@@ -0,0 +1,52 @@
+const refs = {
+  homeButton: document.querySelector('[data-scroll-home]'),
+  reviewsButton: document.querySelector('[data-scroll-reviews]'),
+  aboutButton: document.querySelector('[data-scroll-about]'),
+  productsButton: document.querySelector('[data-scroll-products]'),
+  contactsButton: document.querySelector('[data-scroll-contacts]'),
+  productsHeroButton: document.querySelector('[data-scroll-products-hero]'),
+  aboutHeroButton: document.querySelector('[data-scroll-about-hero]'),
+  homeButtonMenu: document.querySelector('[data-scroll-home-menu'),
+  reviewsButtonMenu: document.querySelector('[data-scroll-reviews-menu]'),
+  aboutButtonMenu: document.querySelector('[data-scroll-about-menu]'),
+  productsButtonMenu: document.querySelector('[data-scroll-products-menu]'),
+  contactsButtonMenu: document.querySelector('[data-scroll-contacts-menu]'),
+  homeButtonBody: document.querySelector('[data-scroll-home-body]'),
+  homeOffset: document.getElementById('hero').offsetTop,
+  reviewsOffset: document.getElementById('reviews').offsetTop,
+  aboutOffset: document.getElementById('about').offsetTop,
+  productsOffset: document.getElementById('products').offsetTop,
+  contactsOffset: document.getElementById('contacts').offsetTop,
+};
+
+refs.homeButton.addEventListener('click', scrollTo(refs.homeOffset));
+refs.reviewsButton.addEventListener('click', scrollTo(refs.reviewsOffset));
+refs.aboutButton.addEventListener('click', scrollTo(refs.aboutOffset));
+refs.productsButton.addEventListener('click', scrollTo(refs.productsOffset));
+refs.contactsButton.addEventListener('click', scrollTo(refs.contactsOffset));
+
+refs.productsHeroButton.addEventListener(
+  'click',
+  scrollTo(refs.productsOffset)
+);
+refs.aboutHeroButton.addEventListener('click', scrollTo(refs.aboutOffset));
+
+refs.homeButtonMenu.addEventListener('click', scrollTo(refs.homeOffset));
+refs.reviewsButtonMenu.addEventListener('click', scrollTo(refs.reviewsOffset));
+refs.aboutButtonMenu.addEventListener('click', scrollTo(refs.aboutOffset));
+refs.productsButtonMenu.addEventListener(
+  'click',
+  scrollTo(refs.productsOffset)
+);
+refs.contactsButtonMenu.addEventListener(
+  'click',
+  scrollTo(refs.contactsOffset)
+);
+
+function scrollTo(offsetTop) {
+  window.scrollTo({
+    top: offsetTop,
+    left: 100,
+    behavior: 'smooth',
+  });
+}
