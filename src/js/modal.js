bf35a34bc7682f39bfd37abe2e59b85e67d6dd6a
Reinghord <105374551+Reinghord@users.noreply.github.com>
2022-07-05 08:26:50 +0300

    added anchors and modal to header

--- src/js/modal.js
+++ src/js/modal.js
@@ -5,20 +5,25 @@
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
     openModalBtnHdr: document.querySelector('[data-modal-header-open]'),
+    openModalBtnHdrScnd: document.querySelector(
+      '[data-modal-header-open-second]'
+    ),
     closeModalBtnHdr: document.querySelector('[data-modal-header-close]'),
     modalHeader: document.querySelector('[data-modal-header]'),
     openModalBtnAbt: document.querySelector('[data-modal-about-open]'),
     closeModalBtnAbt: document.querySelector('[data-modal-about-close]'),
     modalAbout: document.querySelector('[data-modal-about]'),
+    menu: document.querySelector('[data-menu]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.openModalBtnScnd.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
-  refs.openModalBtnHdr.addEventListener('click', toggleModalHeader);
-  refs.closeModalBtnHdr.addEventListener('click', toggleModalHeader);
   refs.openModalBtnAbt.addEventListener('click', toggleModalAbt);
   refs.closeModalBtnAbt.addEventListener('click', toggleModalAbt);
+  refs.openModalBtnHdr.addEventListener('click', toggleModalHeader);
+  refs.openModalBtnHdrScnd.addEventListener('click', toggleModalHeaderSecond);
+  refs.closeModalBtnHdr.addEventListener('click', toggleModalHeader);
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
@@ -27,6 +32,12 @@
   function toggleModalHeader() {
     document.body.classList.toggle('modal-open');
     refs.modalHeader.classList.toggle('is-hidden');
+  }
+  function toggleModalHeaderSecond() {
+    document.body.classList.toggle('modal-open');
+    document.body.classList.remove('no-scroll');
+    refs.modalHeader.classList.toggle('is-hidden');
+    refs.menu.classList.add('is-hidden');
   }
   function toggleModalAbt() {
     document.body.classList.toggle('modal-open');
